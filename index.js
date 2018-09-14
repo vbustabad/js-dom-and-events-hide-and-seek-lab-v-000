721b49fc3b9e480f22669d356b8466407c30b1fd
Veronica Bustabad <vbustabad@gmail.com>
2018-09-14 20:59:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
  }
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelectorAll('target')
+ }
+
+ // function increaseRankBy(n) {
+ //   return document.querySelector(selector);
+ // }
